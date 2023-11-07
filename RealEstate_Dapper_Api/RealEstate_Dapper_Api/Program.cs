b5e3d65efdb7649631356998fa6d7bc64067e5f8
Gülsüm İkbal Avşar <gulsumikbalavsar@gmail.com>
2023-11-07 21:43:39 +0300

    Partial Views Added

--- RealEstate_Dapper_Api/RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_Api/Program.cs
@@ -1,5 +1,6 @@
 using RealEstate_Dapper_Api.Models.DapperContext;
 using RealEstate_Dapper_Api.Models.Repositories.CategoryRepository;
+using RealEstate_Dapper_Api.Models.Repositories.ProductRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<Context>();
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 
 //
---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/ViewComponents/Layout/_HeaderViewComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Layout
+{
+    public class _HeaderViewComponentPartial : ViewComponent //ViewComponent'ler partial view görevi görür
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
