4244221d95a9d6675a1e1b8a1e1387f69d579b7d
Gülsüm İkbal Avşar <gulsumikbalavsar@gmail.com>
2023-11-20 20:29:36 +0300

    Icons of statistics page are edited.

--- RealEstate_Dapper_Api/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9178d4412615eac72aba04989584f714bd2705a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a61ba886f7918d47d843a05e3ce9c4b1053fa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
